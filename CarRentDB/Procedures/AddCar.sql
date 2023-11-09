018a29c97aa63ec418a391399a80f1d983c3d806
jakub-czerniak <j.deadyet@protonmail.com>
2023-11-09 12:22:39 +0100

    Created empty porcedures. AddCar procedure written not checked.

---
+++ CarRentDB/Procedures/AddCar.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE AddCar 
+(
+Price IN NUMBER(6,2),
+RegistrationNumber IN NUMBER,
+Make IN NVARCHAR(255),
+Model IN NVARCHAR(255),
+Localisation IN NVARCHAR(255)
+) 
+AS
+  varMakeId NUMBER;
+  varRentalId
+  localisation_missing EXCEPTION;
+
+BEGIN
+  IF EXISTS(SELECT Id, Make, Model FROM Make WHERE Make = @Make AND Model = @Model)
+    THEN 
+      varMakeId = Id
+    ELSE
+      INSERT INTO Make (Make, Model) VALUES (@Make, @Model) RETURNING Id INTO varMakeId
+  END IF;
+
+  SELECT Id INTO varRentalId FROM Rental WHERE Localisation = @Localisation;
+
+  IF varRentalId IS NULL THEN RAISE localisation_missing;
+  END IF;
+
+  INSERT INTO Car (Price, RegistrationNumber, MakeId, RentalId) VALUES (@Price, @RegistrationNumber, varMakeId, varRentalId)
+
+  NULL;
+
+  WHEN localisation_missing THEN
+    dbms_output.put_line('Localisation does not correspond to any rental.');
+
+END AddCar;
---
+++ CarRentDB/Procedures/CarRents.sql
@@ -0,0 +1,8 @@
+﻿-- Create a new Relational table with 3 columns
+
+CREATE TABLE RELATIONALTABLE1 
+(
+  Column1 VARCHAR2(255) NOT NULL,
+  Column2 VARCHAR2(1024),
+  Column3 NUMBER(3)
+);
