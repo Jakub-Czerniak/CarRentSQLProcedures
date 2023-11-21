0d3b7b73156b55193328cf5df5b0659a5a6b209b
disappointedButNotSuprised <submarine@disroot.org>
2023-11-21 22:22:03 +0100

    add missing procedures

--- CarRentDB/Procedures/CustomerRents.sql
+++ CarRentDB/Procedures/CustomerRents.sql
@@ -1,37 +1,12 @@
-﻿-- Procedure definition
-
-CREATE PROCEDURE PROCEDURE1 (
-  PARAM1 IN NUMBER) IS
-
--- Declare constants and variables in this section.
--- Example: <Variable Identifier> <DATATYPE>
---          <Variable Identifier> CONSTANT <DATATYPE>
---          varEname  VARCHAR2(40);
---          varComm   REAL;
---          varSalary CONSTANT NUMBER:=1000;
---          comm_missing EXCEPTION;
-  varSum NUMBER;
-
--- Executable part starts here
+CREATE PROCEDURE CustomerRents
+(
+  CustomerId in NUMBER
+  Result OUT SYS_REFCURSOR
+) 
+AS
 BEGIN
-
-  -- Write PL/SQL and SQL statements to implement the processing logic
-  -- of subprogram. Example:
-  --     SELECT ENAME,
-  --            COMM
-  --     INTO   varEname,
-  --            varComm
-  --     FROM   EMP
-  --     WHERE  EMPNO = 7369;
-  --
-  --     IF varComm IS NULL THEN
-  --         RAISE comm_missing;
-  --     END IF;
-
-  NULL;
-
-  -- EXCEPTION -- exception-handling part starts here
-  -- WHEN comm_missing THEN
-  --   dbms_output.put_line('Commision is NULL');
-
-END PROCEDURE1;
+  OPEN Result FOR
+  SELECT Customer.Name, Customer.Surname, CarRent.RentDate, CarRent.ExpectedReturnDate, CarRent.CarId
+    FROM Customer
+    INNER JOIN CarRent ON Customer.id = CarRent.CustomerId
+END CustomerRents;
